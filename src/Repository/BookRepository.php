9392da518fe80b7d2f969225906928318d9d9e37
emna <EMNA.FOURATI@esprit.tn>
2025-10-25 15:05:40 +0100

    atelierQB

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -24,6 +24,42 @@ class BookRepository extends ServiceEntityRepository
         ->getResult();
 }
 
+public function booksListByAuthors(): array
+{
+    return $this->createQueryBuilder('b')
+        ->join('b.author', 'a')
+        ->addSelect('a')
+        ->orderBy('a.name', 'ASC')
+        ->getQuery()
+        ->getResult();
+}
+
+public function booksBefore2023WithAuthorsMoreThan10(): array
+{
+    return $this->createQueryBuilder('b')
+        ->join('b.author', 'a')
+        ->addSelect('a')
+        ->where('b.published = true')
+        ->andWhere('b.year < :year')
+        ->andWhere('a.nb_books > :nb')
+        ->setParameter('year', 2023)
+        ->setParameter('nb', 10)
+        ->getQuery()
+        ->getResult();
+}
+
+
+public function updateCategoryFromScienceFictionToRomance(): int
+{
+    return $this->createQueryBuilder('b')
+        ->update()
+        ->set('b.category', ':newCategory')
+        ->where('b.category = :oldCategory')
+        ->setParameter('newCategory', 'Romance')
+        ->setParameter('oldCategory', 'Science-Fiction')
+        ->getQuery()
+        ->execute();
+}
 
 //    /**
 //     * @return Book[] Returns an array of Book objects
